b05fde4e63080607f335a3832b4cf638291cf71e
Harisha Swaminathan <hswamina@gmu.edu>
2021-05-01 03:28:27 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+let output = [];
+const inputs = [5,155, 0987];
+
+const names = {0:'Zero', 1:'One', 2:'Two', 3:'Three', 4:'Four', 5:'Five', 6:'Six', 7:'Seven', 8:'Eight', 9:'Nine'};
+inputs.forEach((input)=>{
+    if(input<10){
+        output.push(names[input]);
+    }else{
+        let result='';
+        while(input>9){
+            result = ''.concat(names[input%10], result)
+            input = Math.floor(input/10);
+        }
+        result = ''.concat(names[input], result,);
+        output.push(result);
+    }
+});
+console.log(output);
